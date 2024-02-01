95dcc54478ff6251e101728cbaae29d0060ec6b2
gegam <gegamus@ya.ru>
2024-02-01 19:21:02 +0300

    уточнение пр деда

---
+++ Цикл for, cамостоятельная работа.py
@@ -0,0 +1,8 @@
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for my_car in cars:
+    cars_count = 0
+    for i in range(10):
+        cars_count += 10
+        print('Я езжу на автомабиле марки', my_car)
+
+
