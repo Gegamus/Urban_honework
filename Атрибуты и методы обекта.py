347545e94e68f76df22e3f933654b5e5735a42a9
gegam <gegamus@ya.ru>
2024-03-17 11:25:34 +0300

    123

--- Атрибуты и методы обекта.py
+++ Атрибуты и методы обекта.py
@@ -3,7 +3,7 @@ class House:
     def __init__(self):
         self.numberOfFloors = 10
 
-        for i in range(10):
+        for i in range(self.numberOfFloors):
             print("Текущий этаж равен", i + 1)
 
 House()
