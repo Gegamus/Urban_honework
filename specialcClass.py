a75175f4f4fe406aded636a768a6e7ac51d5662b
gegam <gegamus@ya.ru>
2024-03-17 11:48:37 +0300

    123

---
+++ specialcClass.py
@@ -0,0 +1,10 @@
+class House:
+
+    def __init__(self):
+        self.numberOfFloors = 0
+
+        def setNewNumberOfFloors(floors):
+            self.numberOfFloors == floors
+
+        print(self.numberOfFloors)
+House()
