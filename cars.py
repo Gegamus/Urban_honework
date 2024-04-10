073f0f6e2e348152d08b92086825e06ea560dc5e
gegam <gegamus@ya.ru>
2024-04-10 23:39:52 +0300

    123

--- cars.py
+++ cars.py
@@ -8,20 +8,14 @@ class Car:
 
 
 class Nissan(Car):
-
-    def __init__(self):
-        super().__init__()
-        self.price = 1800000
+    price = 1800000
 
     def horse_powers(self):
         return 250
 
 
 class Kia(Car):
-
-    def __init__(self):
-        super().__init__()
-        self.price = 1250000
+    price = 1250000
 
     def horse_powers(self):
         return 115
